dc87fefa1a02180356b417b4f33eceb93291afa8
jeantho1 <jeanthomasringeval@hotmail.fr>
2018-05-12 22:59:03 +0200

    Création de 4 objets qui décrivent l'équation entrée

---
+++ ChemicalSolver/chemical_class.py
@@ -0,0 +1,30 @@
+class Equation(object):
+    def __init__(self, seq_str):
+        self.seq = seq_str
+
+    def parse_seq(self):
+        pass
+
+
+class EquationSide(object):
+    def __init__(self, half_seq_str):
+        self.half_seq = half_seq_str
+
+    def parse(self):
+        pass
+
+
+class Molecule(object):
+    def __init__(self, molecule_str):
+        self.molecule = molecule_str
+
+    def parse(self):
+        pass
+
+
+class Atom(object):
+    def __init__(self, atom_str):
+        self.atom = atom_str
+
+    def parse(self):
+        pass
