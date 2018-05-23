9671005de228b7f9fcc8edfb5ec194f937b34aac
desbyp <phildbyp@gmail.com>
2018-05-23 22:41:15 +0200

    add comments

--- ChemicalSolver/chemical_class.py
+++ ChemicalSolver/chemical_class.py
@@ -7,13 +7,16 @@ class Equation(object):
         assert re.fullmatch('[A-Za-z0-9+ ]+->[A-Za-z0-9+ ]+', seq_str), "Invalid equation: {}".format(seq_str)
 
         self.seq = seq_str
-        self.left_eq = None
-        self.right_eq = None
+        self.left_eq = None     # object EquationSide representing the left part
+        self.right_eq = None    # object EquationSide representing the right part
 
     def parse_seq(self):
         parse = self.seq.replace(" ", "").split("->", 1)
-        self.left_eq = EquationSide(parse[0])
-        self.right_eq = EquationSide(parse[1])
+        left_eq_str = parse[0]
+        right_eq_str = parse[1]
+        self.left_eq = EquationSide(left_eq_str)
+        self.right_eq = EquationSide(right_eq_str)
+
         self.left_eq.parse()
         self.right_eq.parse()
 
@@ -25,10 +28,11 @@ class Equation(object):
         pass
         return False
 
+
 class EquationSide(object):
     def __init__(self, half_seq_str):
         self.half_seq = half_seq_str
-        self.list_mol = []
+        self.list_mol = []      # list of molecules contained in the half equation
 
     def parse(self):
         list_mol = self.half_seq.split("+")
@@ -40,15 +44,15 @@ class EquationSide(object):
 class Molecule(object):
     def __init__(self, molecule_str):
         self.mol_str = molecule_str
-        self.qty = None
-        self.mol = None
-        self.atoms = None
+        self.qty = None         # stoechiometric number
+        self.mol = None         # string representing the molecule
+        self.atoms = None       # list of atoms in the molecule
 
     def parse(self):
         # extract quantity
         match = re.fullmatch('(?P<qty>[0-9]*)(?P<mol>[A-Za-z0-9]+)', self.mol_str)
         assert match, "Invalid molecule: {}".format(self.mol_str)
-        self.qty = match.group('qty')
+        self.qty = int(match.group('qty'))
         self.mol = match.group('mol')
 
         # extract atoms
@@ -61,10 +65,10 @@ class Molecule(object):
 class Atom(object):
     def __init__(self, atom_str):
         self.atom_str = atom_str
-        self.qty = None
-        self.symbol = None
+        self.qty = None         # number of atoms
+        self.symbol = None      # string representing the atom letter
 
     def parse(self):
         match = re.fullmatch('(?P<symbol>[A-Za-z]+)(?P<qty>[0-9]*)', self.atom_str)
-        self.qty = match.group('qty')
+        self.qty = int(match.group('qty'))
         self.symbol = match.group('symbol')
