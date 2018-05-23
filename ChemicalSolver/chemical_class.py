82056e80ebee74f257ad7d761ed278b6a9a3075b
desbyp <phildbyp@gmail.com>
2018-05-23 22:45:32 +0200

    fix int parsing

--- ChemicalSolver/chemical_class.py
+++ ChemicalSolver/chemical_class.py
@@ -52,7 +52,7 @@ class Molecule(object):
         # extract quantity
         match = re.fullmatch('(?P<qty>[0-9]*)(?P<mol>[A-Za-z0-9]+)', self.mol_str)
         assert match, "Invalid molecule: {}".format(self.mol_str)
-        self.qty = int(match.group('qty'))
+        self.qty = int(match.group('qty')) if match.group('qty') else 0
         self.mol = match.group('mol')
 
         # extract atoms
@@ -70,5 +70,5 @@ class Atom(object):
 
     def parse(self):
         match = re.fullmatch('(?P<symbol>[A-Za-z]+)(?P<qty>[0-9]*)', self.atom_str)
-        self.qty = int(match.group('qty'))
+        self.qty = int(match.group('qty')) if match.group('qty') else 0
         self.symbol = match.group('symbol')
