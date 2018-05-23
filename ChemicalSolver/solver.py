44cb823a45b76184f2e45a60edc87aeff0d0820e
desbyp <phildbyp@gmail.com>
2018-05-23 22:21:26 +0200

    add simple test

--- ChemicalSolver/solver.py
+++ ChemicalSolver/solver.py
@@ -12,7 +12,12 @@ unbalanced = "H2 + O2 -> H2O"
 
 equation = Equation(unbalanced)
 equation.parse_seq()
-# solution = equation.solve()
+solution = equation.solve()
+
+if solution == "2H2 + O2 -> 2H2O":
+    print("You dit it :)")
+else:
+    print("ERROR: Wrong solution "+format(solution))
 
 # Write an action using print
 # To debug: print("Debug messages...", file=sys.stderr)
